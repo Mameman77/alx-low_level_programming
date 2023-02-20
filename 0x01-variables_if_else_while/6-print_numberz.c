60a276eb0ce751e425edc4bb5e4589611d64d2d2
mame <mameman77@gmail.com>
2023-02-19 18:22:27 -0800

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit
+ *
+ * Return: Always 0 (success/corect)
+ */
+int main(void)
+{
+
+int d;
+
+for (d = 0; d < = 10; d++)
+{
+
+putchar (d);
+
+}
+putchar ('\n');
+
+return (0);
+
+}
+
