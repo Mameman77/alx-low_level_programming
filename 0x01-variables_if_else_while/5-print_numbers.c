57d5785383d6aa64a9b81fac89e58bf516c308bf
mame <mameman77@gmail.com>
2023-02-19 17:59:47 -0800

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints all single digit
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int d;
+
+for (d = 0 ; d < 10; d++)
+{
+printf("%d", d);
+}
+printf("\n");
+return (0);
+}
