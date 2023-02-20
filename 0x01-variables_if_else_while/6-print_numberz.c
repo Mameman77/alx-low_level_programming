92992faefe7cfe9d655cd021421b753d3ef35abc
mame <mameman77@gmail.com>
2023-02-19 20:04:29 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/corect)
+ */
+int main(void)
+
+{
+int d;
+
+for (d = 48 ; d <= 57; d++)
+{
+putchar (d);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
