5dbfebaf595a03818110618bafdd79ecca698e38
mame <mameman77@gmail.com>
2023-02-19 20:14:31 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- * main - print different combinations of two digits.
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success/completed)
  */
 int main(void)
-
 {
 int number_left;
 int number_right;
-
 
 for (number_left = 48; number_right <= 78; number_left++)
 {
@@ -18,7 +16,6 @@ for (number_right = number_left + 1 ; number_right <= 78; number_right++)
 
 putchar(number_left);
 putchar (number_right);
-
 
 if ((number_left == 56) && (number_right == 78))
 {
@@ -29,14 +26,11 @@ putchar(',');
 putchar (' ');
 
 }
-  
 
 }
 
 putchar('\n');
 
-
 return (0);
-
 
 }
